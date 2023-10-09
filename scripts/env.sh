22a79c1ebdfdc6bff818b3894e8390111a78f697
Alexander Slessor <alexjslessor@gmail.com.com>
2023-10-09 13:33:31 -0400

    refactor scripts and poetry lock

---
+++ scripts/env.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set_env() {
+  local env_file_path="$1"
+  # grep -v '^#' $env_file_path
+  export $(grep -v '^#' $env_file_path | xargs)
+  echo "<---ENV: $env_file_path --->"
+}
+
+dev_env_lib() {
+  set_env .env/.base
+#   set_env .env/.base.360
+  echo "${MONGO_DB_NAME}"
+}
+
